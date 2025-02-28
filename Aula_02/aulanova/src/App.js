d3968920dca3a02a0c59dcc3fc964980599e9ef4
RUBENSNETO0001 <RUBENSNETOMARTINSSUAREZNETO@GMAIL.COM>
2025-02-28 14:00:50 -0300

    aula02

--- Aula_02/aulanova/src/App.js
+++ Aula_02/aulanova/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Helloworld from './components/helloworld';
 import Meunome from './components/Meunome'
 import Anime from './components/anime'
+import Frase from './components/Frase'
 
 const nono = 'joao'
 function App() {
@@ -12,6 +13,8 @@ function App() {
       <Meunome nome={nono}/>
       <span>outro componente</span>
       <Anime nome = 'Luffy' idade = '20' profissao = 'Capitão do chapel de palha' foto = 'https://sm.ign.com/ign_br/screenshot/default/imagem-2023-12-15-163503393_5d5p.jpg'/>
+      <Frase />
+      <Frase />
     </div>
   );
 }
--- Aula_02/aulanova/src/components/Frase.js
+++ Aula_02/aulanova/src/components/Frase.js
@@ -1,6 +1,8 @@
+import styles from './Frase.module.css'
+
 function Frase() {
     return (
-        <div>
+        <div className={styles.fraseconteiner}>
             <h1>jurimeu ta indo ate o ritmo do mundo novo do outro lado do mundo onde o mundo e escuro e a maldade e clara</h1>
         </div>
     )
